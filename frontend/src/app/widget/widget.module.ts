cf4086914070c4d1a6311d1ff769f739c53c4af2
kkachmar <71847954+kkachmar@users.noreply.github.com>
2021-11-17 20:43:34 -0500

    tracking-widget

--- frontend/src/app/widget/widget.module.ts
+++ frontend/src/app/widget/widget.module.ts
@@ -1,10 +1,13 @@
+import { ButtonModule } from 'primeng/button';
 import { SmallWidgetComponent } from './small-widget/small-widget.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BasicWidgetComponent } from './basic-widget/basic-widget.component';
 import {SliderModule} from 'primeng/slider';
 import { ReceiptTrackingComponent } from './receipt-tracking/receipt-tracking.component';
-
+import { TrackingComponent } from './tracking/tracking.component';
+import { InputTextModule } from 'primeng/inputtext';
+import {CalendarModule} from 'primeng/calendar'
 
 
 
@@ -12,12 +15,16 @@ import { ReceiptTrackingComponent } from './receipt-tracking/receipt-tracking.co
   declarations: [
     BasicWidgetComponent,
     SmallWidgetComponent,
-    ReceiptTrackingComponent
+    ReceiptTrackingComponent,
+    TrackingComponent
   ],
   imports: [
     CommonModule,
-    SliderModule
+    SliderModule,
+    ButtonModule,
+    InputTextModule,
+    CalendarModule
   ],
-  exports: [BasicWidgetComponent, SmallWidgetComponent, ReceiptTrackingComponent]
+  exports: [BasicWidgetComponent, SmallWidgetComponent, ReceiptTrackingComponent, TrackingComponent]
 })
 export class WidgetModule { }
