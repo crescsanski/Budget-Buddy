3883d3ffff1fdff261f9b90ea5e86f7f2e5c0559
kkachmar <71847954+kkachmar@users.noreply.github.com>
2022-02-10 16:04:06 -0500

    net income widget placeholder

---
+++ frontend/src/app/widget/net-income/net-income.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-net-income',
+  templateUrl: './net-income.component.html',
+  styleUrls: ['./net-income.component.scss', './../basic-widget/basic-widget.component.scss']
+})
+export class NetIncomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
