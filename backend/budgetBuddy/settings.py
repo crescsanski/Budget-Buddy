c66994bc6c595dc655fa25822f3cfd0a0acf93eb
crescsanski <cjrescsanski@quinnipiac.edu>
2022-04-25 11:28:48 -0400

    Keep authentication

--- backend/budgetBuddy/settings.py
+++ backend/budgetBuddy/settings.py
@@ -77,7 +77,7 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.JSONRenderer',
     ),
      'DEFAULT_PERMISSION_CLASSES': [
-        # 'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.IsAuthenticated',
      ]
 }
 
